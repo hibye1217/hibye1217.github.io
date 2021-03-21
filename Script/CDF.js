7d66a5070219a56d4f179ed34c21ded4676b08d2
hibye1217 <mathking2032@gmail.com>
2021-03-21 21:17:36 +0900

    Commit #146
    [hi12] CDF 1452C 풀이 수정
    
    A̴̠̐c̸͉̽ċ̷̙e̵̙͊s̵̟̑s̵̭͝ ̵̯̌C̸̢̈́ơ̵͉d̵͈͊ë̶̺́:̸̼̿ ̵̠̄2̶̠͘-̶͖̈́7̸̘͛3̸̢͂

--- Script/CDF.js
+++ Script/CDF.js
@@ -1,3 +1,3 @@
 function AddArticles(){
-    PushArticle("Two Brackets", "./CDF/1452C.html", "1452C", [ "*800", "ECR98" ]);
+    PushArticle("Two Brackets", "./CDF/1452C.html", "1452C", [ "800", "ECR98" ]);
 }

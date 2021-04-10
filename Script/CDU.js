f61671f3b212c2de72aaed48fb5ecbd681967c24
hibye1217 <mathking2032@gmail.com>
2021-04-10 23:28:18 +0900

    Commit #153
    링크 또 빼먹었다
    
    Á̶̝ç̴͊̒c̴̩̄͛e̵̢̋̔s̵͎̈s̴̘̀͒ ̶̟̑C̵̗̅ͅơ̴̯͝ḏ̶̡͌̉ḛ̷̃:̵̫̓̆ ̸͖̐3̸̣͚̓-̷̬̝͐͝3̵̼̄-̸̫̓̕1̷͈̘͒7̸̺̄̏

--- Script/CDU.js
+++ Script/CDU.js
@@ -1,3 +1,4 @@
 function AddArticles(){
+    PushArticle("2차원 배열 달팽이 채우기 4-6", "./CDU/1489.html", "1489", [ "기초-배열연습" ]);
     PushArticle("정수 2개 입력받아 비교하기 1", "./CDU/6048.html", "6048", [ "Python 기초 100제", "비교연산" ]);
 }

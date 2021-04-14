f6114678f65cd9827c606a3c57462fd1dc83ca21
hibye1217 <mathking2032@gmail.com>
2021-04-14 22:12:48 +0900

    Commit #157
    아 링크 다는 거 또 깜빡했다
    
    E̴r̴r̶o̶r̷ ̴1̵5̵7̵:̸ ̵C̶a̸n̵n̶o̶t̶ ̶f̶i̵n̵d̴ ̶a̶n̶y̶ ̷f̶a̵c̵t̷o̸r̷ ̶o̶t̶h̶e̴r̴ ̴t̴h̸a̸n̶ ̴i̷t̵s̷e̶l̸f̵.̵

--- Script/ATC.js
+++ Script/ATC.js
@@ -1,3 +1,4 @@
 function AddArticles(){
+    PushArticle("Choose Me", "./ATC/ABC187D.html", "ABC187D", [ "ABC187" ]);
     PushArticle("Remove It", "./ATC/ABC191B.html", "ABC191B", [ "ABC191" ]);
 }

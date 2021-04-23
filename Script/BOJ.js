13357df92464c373c90b8e9e3f9a761b7a83a630
hibye1217 <mathking2032@gmail.com>
2021-04-23 22:09:01 +0900

    Commit #159
    BOJ Error Fixed

--- Script/BOJ.js
+++ Script/BOJ.js
@@ -1,5 +1,5 @@
 function AddArticles(){
-    PushArticle("무한 수열", "./BOJ/1351.html", [ "무한 수열" ]);
+    PushArticle("무한 수열", "./BOJ/1351.html", "1351", [ "무한 수열" ]);
     PushArticle("숨바꼭질", "./BOJ/1697.html", "1697", [ "숨바꼭질", "CLASS 3 에센셜" ]);
     PushArticle("회의실 배정", "./BOJ/1931.html", "1931", [ "CLASS 3 에센셜" ]);
 }

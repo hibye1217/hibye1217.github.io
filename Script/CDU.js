86b373e0657aa993cdde0bbbf460ef6d51366d4d
hibye1217 <mathking2032@gmail.com>
2021-04-24 10:33:17 +0900

    Commit #160
    [BOJ 1351]
    [CodeUp 4536]
    
    b̵͖̅ỳ̸̰e̵̛͈1̶͙͠7̴̯̐'̸̝͛ṣ̶̃ ̷̹̐L̶̡͆ö̵̟́g̷̘̀.̷̲̏ ̸̻̚2̶̲̈-̷͙́2̵̦̄-̴̫̔2̵̥͝-̶̳̿2̷͈̀-̶͓͂2̶͕͛-̸̦̐5̷̱͂:̷̮̒ ̷͔̒Ẅ̶̥́h̶͖̾ö̷͓́.̸̩̀.̵̣̚.̸̨̀ ̷͚̊A̴͚̓m̸͍͗.̵̪͠.̸̪̌.̸̠̃ ̵͎̒I̵͈̕?̵̭͊

--- Script/CDU.js
+++ Script/CDU.js
@@ -1,4 +1,5 @@
 function AddArticles(){
     PushArticle("2차원 배열 달팽이 채우기 4-6", "./CDU/1489.html", "1489", [ "기초-배열연습" ]);
     PushArticle("정수 2개 입력받아 비교하기 1", "./CDU/6048.html", "6048", [ "Python 기초 100제", "비교연산" ]);
+    PushArticle("대표값", "./CDU/4536.html", "4536", [ "정올 1번 수준 문제" ])
 }
--- Script/module/index.js
+++ Script/module/index.js
@@ -1,3 +1,17 @@
 function isInt(x){
     return !( isNaN(x) || x.length == 0 || x.indexOf('.') != -1 || x.indexOf('e') != -1 )
 }
+
+function getParamsFromLink(){
+    let link = location.href;
+    let str = link.substring( link.indexOf('?')+1 );
+    let url = new URLSearchParams(str);
+    let params = [];
+    for (const [key, val] of url){
+        params.push({
+            key: key,
+            val: val
+        });
+    }
+    return params;
+}

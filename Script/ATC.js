c0d2cf5525ac8ad43bbe332d58f0973c2bb37696
hibye1217 <mathking2032@gmail.com>
2021-05-08 12:47:08 +0900

    Commit #169
    머야 벌써 13의 제곱이네

--- Script/ATC.js
+++ Script/ATC.js
@@ -2,4 +2,5 @@ function AddArticles(){
     PushArticle("Choose Me", "./ATC/ABC187D.html", "ABC187D", [ "ABC187" ]);
     PushArticle("Two Sequences 2", "./ATC/KEYENCE2021A", "KEYENCE2021A", [ "Keyence 2021" ])
     PushArticle("Remove It", "./ATC/ABC191B.html", "ABC191B", [ "ABC191" ]);
+    PushArticle("Difference Max", "./ATC/ABC196A.html", "ABC196A", [ "ABC196" ]);
 }
